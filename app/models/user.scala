01e93ba97329806c2b7ad1118f702c7f6543caa7
ranjithraj.d <ranjithraj.d@gmail.com>
2016-04-10 13:10:47 +0530

    created_date and update_date added

--- app/models/user.scala
+++ app/models/user.scala
@@ -1,6 +1,7 @@
 package models
 
-import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
+import org.joda.time.{DateTimeZone, DateTime}
 
 import reactivemongo.bson._
 
@@ -10,7 +11,9 @@ case class User( _id : Option[BSONObjectID],
                  age: Int,
                  firstName: String,
                  lastName: String,
-                 active: Boolean
+                 active: Boolean,
+                 createdDate: Option[DateTime],
+                 updatedDate: Option[DateTime]
                 )
 import play.api.libs.json.Json
 import play.api.libs.json._
@@ -29,20 +32,39 @@ object User {
       "$oid" -> objectId.stringify
     )
   }
+  implicit val dateTimeRead: Reads[DateTime] =
+    (__ \ "$date").read[Long].map { dateTime =>
+      new DateTime(dateTime, DateTimeZone.UTC)
+    }
+
+
+  implicit val dateTimeWrite: Writes[DateTime] = new Writes[DateTime] {
+    def writes(dateTime: DateTime): JsValue = Json.obj(
+      "$date" -> dateTime.getMillis
+    )
+  }
+
+  implicit val objectIdFormats = Format(objectIdRead, objectIdWrite)
+  implicit val dateTimeFormats = Format(dateTimeRead, dateTimeWrite)
   // User JSON Format implicit for REST
   implicit val userReads: Reads[User] = (
   (JsPath \ "_id").readNullable[BSONObjectID] and
   (JsPath \ "age").read[Int] and
   (JsPath \ "firstName").read[String] and
   (JsPath \ "lastName").read[String] and
-  (JsPath \ "active").read[Boolean]
+  (JsPath \ "active").read[Boolean] and
+  (JsPath \ "createdDate").readNullable[DateTime].map(_.getOrElse(new DateTime())).map(Some(_))  and
+  (JsPath \ "updatedDate").readNullable[DateTime].map(_.getOrElse(new DateTime())).map(Some(_))
 )(User.apply _)
+  val userDateFormatter = DateTimeFormat.forPattern("MM/dd/yyyy HH:mm a")
   implicit val userWrites: Writes[User] = (
   (JsPath \ "_id").writeNullable[BSONObjectID]   and
   (JsPath \ "age").write[Int] and
   (JsPath \ "firstName").write[String] and
   (JsPath \ "lastName").write[String] and
-  (JsPath \ "active").write[Boolean]
+  (JsPath \ "active").write[Boolean] and
+  (JsPath \ "createdDate").writeNullable[DateTime] and
+  (JsPath \ "updatedDate").writeNullable[DateTime].contramap[DateTime](dt => userDateFormatter.print(dt))
 )(unlift(User.unapply))
   implicit val userFormatFromJSON =Json.fromJson[User]
   implicit val userFormatToJSON =Json.toJson[User]
