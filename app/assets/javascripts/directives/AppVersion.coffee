1e24b12134f48d5e16629865ca1cafb3a5d4f0ed
Venkat ramanan <venkatramanan.s@imaginea.com>
2016-04-15 18:18:32 +0530

    Changes in Admin tab - date and other fields added

--- app/assets/javascripts/directives/AppVersion.coffee
+++ app/assets/javascripts/directives/AppVersion.coffee
@@ -26,3 +26,40 @@ directivesModule.directive 'splitArray', ->
       return
 
   }
+
+
+directivesModule.directive 'dateFrmt', ->
+  {
+    restrict: 'A'
+    require: 'ngModel'
+    link: (scope, element, attr, ngModel) ->
+
+      fromUser = (date) ->
+        console.log('from-dateFrmtaa'+date)
+        year = date.getFullYear()
+        month = forceTwoDigits(date.getMonth())
+        day = forceTwoDigits(date.getDate()+1)
+        console.log(month+'-finlfrom-'+year+'--'+day)
+        return new Date(year, month, day)
+
+
+      toUser = (date) ->
+        date = new Date(date)
+        console.log('to-dateFrmt'+date)
+        year = date.getFullYear()
+        month = date.getMonth()
+        day = date.getDate()
+        console.log(month+'-finlaa-'+year+'--'+day)
+        return new Date(year, month, day)
+
+      forceTwoDigits = (val) ->
+        if val < 10
+          return "0#{val}"
+        return val
+
+
+      ngModel.$parsers.push fromUser
+      ngModel.$formatters.push toUser
+      return
+
+  }
--- app/models/user.scala
+++ app/models/user.scala
@@ -1,14 +1,18 @@
 package models
 
+import java.util.Date
+
 import org.joda.time.format.DateTimeFormat
 import org.joda.time.{DateTimeZone, DateTime}
-
 import reactivemongo.bson._
 
 case class User( _id : Option[BSONObjectID],
                  age: Int,
+                 dob : Date,
                  firstName: String,
                  lastName: String,
+                 emailId: String,
+                 interest: String,
                  active: Boolean,
                  createdDate: Option[DateTime],
                  updatedDate: Option[DateTime]
@@ -25,8 +29,11 @@ object User {
   implicit val userReads: Reads[User] = (
   (JsPath \ "_id").readNullable[BSONObjectID] and
   (JsPath \ "age").read[Int] and
+  (JsPath \ "dob").read[Date] and
   (JsPath \ "firstName").read[String] and
   (JsPath \ "lastName").read[String] and
+  (JsPath \ "emailId").read[String] and
+  (JsPath \ "interest").read[String] and
   (JsPath \ "active").read[Boolean] and
   (JsPath \ "createdDate").readNullable[DateTime].map(_.getOrElse(new DateTime())).map(Some(_))  and
   (JsPath \ "updatedDate").readNullable[DateTime].map(_.getOrElse(new DateTime())).map(Some(_))
@@ -35,8 +42,11 @@ object User {
   implicit val userWrites: Writes[User] = (
   (JsPath \ "_id").writeNullable[BSONObjectID]   and
   (JsPath \ "age").write[Int] and
+  (JsPath \ "dob").write[Date] and
   (JsPath \ "firstName").write[String] and
   (JsPath \ "lastName").write[String] and
+  (JsPath \ "emailId").write[String] and
+  (JsPath \ "interest").write[String] and
   (JsPath \ "active").write[Boolean] and
   (JsPath \ "createdDate").writeNullable[DateTime] and
   (JsPath \ "updatedDate").writeNullable[DateTime]
